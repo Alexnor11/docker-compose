918e24920f24b304bf1ec554e781e068f667715d
Alexnor <advance114@gmail.com>
2022-11-23 22:06:53 +0300

    add home_url

--- stocks_products/urls.py
+++ stocks_products/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('logistic.urls')),
-    path('', index),
+    path('home/', index),
 ]
 
 # Подключение статических файлов к проекту
